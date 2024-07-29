7183f712d13df09dd6c1f04cbfece7ac8e4d262a
Gesang Paudra J <g.paudra@gmail.com>
2024-07-29 22:03:59 +0700

    Create migration.sql

---
+++ prisma/migrations/20240729144317_hapus_gambar_produk/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `gambar` on the `Produk` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Produk" DROP COLUMN "gambar";
