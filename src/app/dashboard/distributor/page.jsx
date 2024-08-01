3fd4bcccab55a95deefd63f6fe2e6b3d88b8bc35
Gesang Paudra J <g.paudra@gmail.com>
2024-08-01 21:38:04 +0700

    buat halaman daftar distributor

---
+++ src/app/dashboard/distributor/page.jsx
@@ -0,0 +1,45 @@
+// Halaman Daftar Distributor
+
+"use client"
+
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import TabelDistributor from '@/views/distributor/daftar-distributor'
+
+const HalamanDaftarDistributor = () => {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
+
+  return(
+    <div>
+      <div>
+        <h1 className='text-2xl font-bold'>
+          Daftar Distributor
+        </h1>
+      </div>
+      <br />
+      <div>
+        <TabelDistributor/>
+      </div>
+
+    </div>
+  )
+}
+
+export default HalamanDaftarDistributor
