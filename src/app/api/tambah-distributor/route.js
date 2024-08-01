95a22f31640bfd2bfdf8afb64c4db2623025e418
Gesang Paudra J <g.paudra@gmail.com>
2024-08-01 21:38:34 +0700

    buat API tambah distributor

---
+++ src/app/api/tambah-distributor/route.js
@@ -0,0 +1,55 @@
+// API tambah distributor
+
+import { NextResponse } from 'next/server'
+
+
+import { getToken } from 'next-auth/jwt'
+
+import prisma from '@/app/lib/prisma'
+
+export const POST = async (req) => {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  console.log('Token:', token)
+
+  if (!token) {
+    console.log('Unauthorized Access : API Tambah Distributor')
+
+    return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
+  }
+
+  try{
+    const {nama, notelp, email, alamat} = await req.json()
+
+    if( !nama || !notelp || !email || !alamat){
+      return NextResponse.json({error: "Semua bidang harus diisi."}, {status:400})
+    }
+
+    try{
+      const distributor = await prisma.distributor.create({
+        data:{
+          nama,
+          email,
+          notelp,
+          alamat,
+        },
+      })
+
+      console.log("distributor berhasil ditambahkan :", distributor)
+
+      return NextResponse.json(distributor, {status:201})
+
+    }
+    catch(error){
+      console.error("Error menambahkan data distributor :", error)
+
+      return NextResponse.json({error: "Error menambahkan data distributor"}, {status:400})
+    }
+
+  }
+  catch(error){
+    console.error("Error memproses request", error)
+
+    return NextResponse.json({error:"Error parsing request body"}, {status: 500})
+  }
+}
