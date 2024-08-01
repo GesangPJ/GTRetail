861cf2b8f94ef2e3b1ce212e800f01ce12fa642a
Gesang Paudra J <g.paudra@gmail.com>
2024-08-01 19:15:32 +0700

    update halaman atur stok produk

--- src/app/dashboard/atur-stok/page.jsx
+++ src/app/dashboard/atur-stok/page.jsx
@@ -1,11 +1,40 @@
+'use client'
 
+import { useEffect } from "react"
+
+import { useRouter } from "next/navigation"
+
+import { useSession } from "next-auth/react"
 
 
 
 const HalamanAturStok =() => {
+  const {data: session, status} = useSession()
+  const router = useRouter()
+
+  useEffect(()=>{
+    if (status === 'loading') return
+
+    if(!session){
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if(!session){
+    return null
+  }
 
   return(
     <div>
+      <div>
+        <h1 className="text-2xl font-bold">
+          Pembaruan Stok Produk
+        </h1>
+      </div>
+      <br />
+      <div>
+
+      </div>
 
     </div>
   )
