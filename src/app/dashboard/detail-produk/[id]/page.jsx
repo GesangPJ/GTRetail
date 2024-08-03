c8ba1f2a8bc1123fa063cc4c2c4d69ef3e56a954
Gesang Paudra J <g.paudra@gmail.com>
2024-08-03 17:55:16 +0700

    update halaman detail produk
    menambahkan fungsi edit data produk

--- src/app/dashboard/detail-produk/[id]/page.jsx
+++ src/app/dashboard/detail-produk/[id]/page.jsx
@@ -12,13 +12,18 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
-import { Button } from '@mui/material'
+import { Button, Dialog, DialogActions,
+  DialogContent, DialogContentText, DialogTitle, TextField, MenuItem} from '@mui/material'
 import Box from '@mui/material/Box'
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf'
 import ListAltIcon from '@mui/icons-material/ListAlt'
 import { jsPDF } from "jspdf"
 import autoTable from 'jspdf-autotable'
 import ExcelJS from 'exceljs'
+import Select from '@mui/material/Select'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+
 
 const formatDate = (dateString) => {
   if (!dateString) return 'Invalid Date'
@@ -45,6 +50,39 @@ const DetailPage = () => {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [open, setOpen] = useState(false)
+
+  const [formData, setFormData] = useState({
+    produkId: '',
+    nama: '',
+    harga: '',
+    hargabeli: '',
+    satuan:'',
+    status:'',
+    keterangan:'',
+   })
+
+   const handleClickOpen = (row) => {
+    setFormData({ produkId: data.id,
+    nama: data.nama,
+    harga: data.harga,
+    hargabeli: data.hargabeli,
+    satuan: data.satuan,
+    keterangan: data.keterangan,
+    status: data.status,
+    })
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setFormData({ ...formData, [name]: value })
+  }
 
   useEffect(() => {
     if (status === 'loading') return
@@ -105,6 +143,7 @@ const DetailPage = () => {
     { label: 'Barcode', value: data.barcode},
     { label: 'Nama Produk', value: data.nama },
     { label: 'Harga Produk', value: formatCurrency(data.harga) },
+    { label: 'Harga Beli', value: formatCurrency(data.hargabeli)},
     { label: 'Satuan', value: data.satuan},
     { label: 'Stok', value: data.stok},
     { label: 'Kategori', value: data.kategori},
@@ -146,8 +185,36 @@ const DetailPage = () => {
     document.body.removeChild(link)
   }
 
-  const handleDocxExport = () => {
-    // Implement your Docx export logic here
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('/api/edit-produk', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+
+      const result = await response.json()
+
+      if (response.ok) {
+        alert('Data Admin Berhasil diubah')
+        setRows((prevRows) =>
+          prevRows.map((row) =>
+            row.id === formData.userId
+              ? { ...row, name: formData.nama, email: formData.email }
+              : row
+          )
+        )
+      } else {
+        alert(result.error || 'Ada kesalahan ketika mengganti data akun')
+      }
+
+      handleClose()
+    } catch (error) {
+      console.error('Error mengubah data akun:', error)
+      alert('Ada kesalahan ketika mengganti data akun')
+    }
   }
 
   return (
@@ -180,6 +247,72 @@ const DetailPage = () => {
           Export XLSX
         </Button>
       </Box>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Data Admin</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Masukkan data yang ingin diubah.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="nama"
+            label="Nama Produk"
+            type="text"
+            fullWidth
+            value={formData.nama}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="harga"
+            label="Harga Produk"
+            type="number"
+            fullWidth
+            value={formData.harga}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="hargabeli"
+            label="Harga Beli Produk"
+            type="number"
+            fullWidth
+            value={formData.hargabeli}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="satuan"
+            label="Satuan"
+            type="text"
+            fullWidth
+            value={formData.satuan}
+            onChange={handleChange}
+          />
+          <FormControl fullWidth margin="dense">
+            <InputLabel>Status</InputLabel>
+            <Select
+              name="status"
+              value={formData.status}
+              onChange={handleChange}
+            >
+              <MenuItem value="AKTIF">AKTIF</MenuItem>
+              <MenuItem value="NONAKTIF">NONAKTIF</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" onClick={handleClose} color="error" sx={ { borderRadius: 30 } }>
+            Batal
+          </Button>
+          <Button variant="contained" onClick={handleSubmit} color="primary" sx={ { borderRadius: 30 } }>
+            Ganti Data
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </div>
   )
 }
