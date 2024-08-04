5895062ef9bdff6fcd76a272f7b0f2478f2cb31d
Gesang Paudra J <g.paudra@gmail.com>
2024-08-04 20:28:36 +0700

    buat API ganti status pembelian

---
+++ src/app/api/ganti-status-pembelian/route.js
@@ -0,0 +1,45 @@
+// API ganti status pembelian
+
+import { NextResponse } from 'next/server'
+
+import { getToken } from 'next-auth/jwt'
+
+import { logToFile } from '@/app/lib/logger'
+
+import prisma from '@/app/lib/prisma'
+
+export const PUT = async (req) => {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  console.log('Token:', token)
+
+  if (!token) {
+    console.log('Unauthorized Access : API Ambil Status Pembelian')
+
+    logToFile('Unauthorized Access : API Ambil Status Pembelian')
+
+    return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
+  }
+
+  try{
+    const {pembelianId, status} = await req.json()
+
+    if(!pembelianId || !status){
+      return NextResponse.json({error:"Data tidak boleh kosong"}, {status:400})
+    }
+
+    const datastatus = await prisma.pembelian.update({
+      where: {id:pembelianId},
+      data:{status},
+    })
+
+    return NextResponse.json(datastatus, {status:201})
+
+  }
+  catch(error){
+    console.error("Error parsing request body:", error)
+
+    return NextResponse.json({ error: "Bad Request" }, { status: 400 })
+  }
+
+}
