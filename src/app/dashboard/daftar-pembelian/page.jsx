be32ea6313a08ea946eaac18c33344de13d216ad
Gesang Paudra J <g.paudra@gmail.com>
2024-08-04 09:08:30 +0700

    buat halaman daftar pembelian

---
+++ src/app/dashboard/daftar-pembelian/page.jsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { useEffect, Suspense } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import TabelDaftarPembelian from '@/views/pembelian/daftar-pembelian'
+
+const HalamanDaftarPembelian = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
+
+  return(
+    <div>
+      <div>
+        <h1 className='text-2xl font-bold'>
+          Daftar Pembelian
+        </h1>
+      </div>
+      <br />
+      <Suspense fallback={<div>Memuat Data...</div>}>
+        <div>
+          <TabelDaftarPembelian/>
+        </div>
+      </Suspense>
+
+
+    </div>
+  )
+}
+
+export default HalamanDaftarPembelian
