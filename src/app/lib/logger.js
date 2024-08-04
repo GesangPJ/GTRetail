60bb14cd391aba88ea56d26f776073aea16a7775
Gesang Paudra J <g.paudra@gmail.com>
2024-08-04 14:49:02 +0700

    buat fungsi Logging

---
+++ src/app/lib/logger.js
@@ -0,0 +1,18 @@
+// /src/lib/logger.js
+import fs from 'fs'
+import path from 'path'
+
+// Define the log file path
+const logFilePath = path.join(process.cwd(), 'logs', 'api.log')
+
+// Ensure the logs directory exists
+if (!fs.existsSync(path.dirname(logFilePath))) {
+  fs.mkdirSync(path.dirname(logFilePath), { recursive: true })
+}
+
+export const logToFile = (message) => {
+  const timestamp = new Date().toISOString()
+  const logMessage = `${timestamp} - ${message}\n`
+
+  fs.appendFileSync(logFilePath, logMessage, 'utf8')
+}
