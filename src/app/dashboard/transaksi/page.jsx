9556ec4e504b46be0ec813008a843a69c551e674
Gesang Paudra J <g.paudra@gmail.com>
2024-08-07 21:58:04 +0700

    update halaman transaksi
    menambakan view daftar transaksi

--- src/app/dashboard/transaksi/page.jsx
+++ src/app/dashboard/transaksi/page.jsx
@@ -1,13 +1,41 @@
+'use client'
 
+import { useEffect, Suspense } from 'react'
 
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import TableTransaksi from "@/views/penjualan/daftar-transaksi"
 
 const HalamanTransaksi = () => {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
 
   return(
     <div>
       <h1 className="text-xl font-bold">
-        Halaman Transaksi
+        Daftar Transaksi
       </h1>
+      <br />
+      <Suspense fallback={<div>Memuat Data...</div>}>
+      <div>
+        <TableTransaksi/>
+      </div>
+      </Suspense>
+
     </div>
   )
 }
