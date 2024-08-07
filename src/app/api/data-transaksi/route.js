561b5efc7a6810a170519e0362c8a78e9040f12b
Gesang Paudra J <g.paudra@gmail.com>
2024-08-07 21:58:14 +0700

    buat API ambil data transaksi

---
+++ src/app/api/data-transaksi/route.js
@@ -0,0 +1,57 @@
+// API data transaksi
+
+import { NextResponse } from 'next/server'
+
+import { getToken } from 'next-auth/jwt'
+
+import prisma from '@/app/lib/prisma'
+
+export async function GET(req){
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  console.log('Token:', token)
+
+  if (!token) {
+    console.log('Unauthorized Access : API Data Produk')
+
+    return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
+  }
+
+  const { searchParams } = new URL(req.url)
+  const userId = searchParams.get('userId')
+
+  if (!userId) {
+    return NextResponse.json({ error: 'User ID tidak ditemukan!' }, { status: 400 })
+  }
+
+  console.log('User dengan Id : ',userId,' mengakses API data transaksi')
+
+  try{
+    const datatransaksi = await prisma.transaksi.findMany({
+      select:{
+        id:true,
+        kode:true,
+        namapelanggan:true,
+        status: true,
+        jumlahTotal:true,
+        updatedAt: true,
+        pelanggan:{select:{nama:true,},},
+      }
+    })
+
+    const formattransaksi = datatransaksi.map(transaksi => ({
+      ...transaksi,
+      namaPelanggan: transaksi.pelanggan?.nama || transaksi.namapelanggan || '-',
+      updatedAt: transaksi.updatedAt.toISOString(),
+    }))
+
+    return NextResponse.json(formattransaksi, {status:200})
+  }
+  catch(error){
+    console.error('Error mengambil data transaksi:', error)
+
+    return NextResponse.json({ error: 'Terjadi kesalahan saat mengambil data transaksi' }, { status: 500 })
+
+  }
+
+}
