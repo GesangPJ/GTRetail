ba7483716c7677265aff0d39d2d06dab1b02160a
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 20:45:28 +0700

    Delete migration.sql

--- prisma/migrations/20240801045711_ubah_tipe_harga/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `harga` on the `Produk` table. The data in that column could be lost. The data in that column will be cast from `BigInt` to `Integer`.
-
-*/
--- AlterTable
-ALTER TABLE "Produk" ALTER COLUMN "harga" SET DATA TYPE INTEGER;
