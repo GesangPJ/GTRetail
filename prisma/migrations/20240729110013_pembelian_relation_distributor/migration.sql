dce12787a31ff467199c507ddabb3def1a3286f5
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 20:45:10 +0700

    Delete migration.sql

--- prisma/migrations/20240729110013_pembelian_relation_distributor/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Pembelian" ADD CONSTRAINT "Pembelian_distributorId_fkey" FOREIGN KEY ("distributorId") REFERENCES "Distributor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
