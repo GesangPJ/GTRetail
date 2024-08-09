898defb488d1eaba640f6e400d40bf542f08094c
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 20:46:29 +0700

    Delete migration.sql

--- prisma/migrations/20240805115900_edit_produk/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "jenis_p" AS ENUM ('BARANG', 'PANGAN');
-
--- AlterTable
-ALTER TABLE "Produk" ADD COLUMN     "jenis" "jenis_p" NOT NULL DEFAULT 'BARANG',
-ADD COLUMN     "kadaluarsa" TIMESTAMP(3);
