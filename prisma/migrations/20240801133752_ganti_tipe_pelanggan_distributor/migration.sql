2bd1c6d0a663ae2ee3a0985cf824d5722a87a4d9
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 20:45:36 +0700

    Delete migration.sql

--- prisma/migrations/20240801133752_ganti_tipe_pelanggan_distributor/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Distributor" ALTER COLUMN "notelp" DROP NOT NULL,
-ALTER COLUMN "notelp" SET DATA TYPE TEXT,
-ALTER COLUMN "alamat" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Pelanggan" ALTER COLUMN "notelp" SET DATA TYPE TEXT;
