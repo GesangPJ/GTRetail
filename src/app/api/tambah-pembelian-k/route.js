d64e71a5975009cd4d6a4839d500708eb56a6187
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 21:02:04 +0700

    update API tambah pembelian
    menambahkan fungsi membuat jurnal untuk pembelian

--- src/app/api/tambah-pembelian-k/route.js
+++ src/app/api/tambah-pembelian-k/route.js
@@ -67,6 +67,7 @@ export const POST = async (req) => {
     }
 
     const status = "PENDING"
+    const namaAkun = "BANK"
 
     // Membuat kode baru
     const newKode = `GT/PURCHASE/${currentMonth}/${currentYear}/${newNumber}`
@@ -88,7 +89,17 @@ export const POST = async (req) => {
       },
     })
 
-    return NextResponse.json(newPembelian, { status: 201 })
+    // Membuat jurnal pembelian baru
+    const newJurnal = await prisma.jurnal.create({
+      data: {
+        transaksiId: newTransaksi.id,
+        debit: totalHarga,
+        akun: namaAkun,
+        kode: newKode,
+      },
+    })
+
+    return NextResponse.json(newPembelian, newJurnal, { status: 201 })
 
   } catch (error) {
     console.error('Error menambah pembelian:', error)
