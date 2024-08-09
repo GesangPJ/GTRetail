0f96167a126d08585f818420d0f9f5d661c2e752
Gesang Paudra J <g.paudra@gmail.com>
2024-08-09 20:46:56 +0700

    Delete migration.sql

--- prisma/migrations/20240809115509_tambah_metode_transaksi/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `metode` to the `Transaksi` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Transaksi" ADD COLUMN     "metode" TEXT NOT NULL;
