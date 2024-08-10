b26527f9cc6b07cdbbd5f6a25d084d4f87bcda7b
Gesang Paudra J <g.paudra@gmail.com>
2024-08-10 11:52:58 +0700

    update API ubah stok
    tambah komentar

--- src/app/api/ubah-stok/route.js
+++ src/app/api/ubah-stok/route.js
@@ -12,15 +12,18 @@ export const PUT = async (req) =>{
 
   console.log('Token :', token)
 
+  // cek apakah API diakses dengan menggunakan token
   if(!token){
     console.log('Unauthorized Access : API Edit Karyawan')
 
     return NextResponse.json({error:'Unauthorized Access'}, {status:401})
   }
 
+  // parsing data yang dikirim ke API
   try{
     const {produkId, stok} = await req.json()
 
+    // memasukkan data ke database
     try{
       const gantistok = await prisma.produk.update({
         where: {id:parseInt(produkId)},
@@ -29,6 +32,7 @@ export const PUT = async (req) =>{
         }
       })
 
+      // kirim response berhasil
       return NextResponse.json(gantistok, {status:200})
     }
     catch(error){
