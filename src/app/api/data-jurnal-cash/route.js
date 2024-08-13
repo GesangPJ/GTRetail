626f4dce5c9c23a7132531bd07809e084dec88f6
Gesang Paudra J <g.paudra@gmail.com>
2024-08-13 12:35:42 +0700

    buat API jurnal cash

---
+++ src/app/api/data-jurnal-cash/route.js
@@ -0,0 +1,60 @@
+// API Data Jurnal | Mengambil data jurnal
+
+import { NextResponse } from 'next/server'
+
+import { getToken } from 'next-auth/jwt'
+
+import prisma from '@/app/lib/prisma'
+
+export async function GET(req){
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  console.log('Token:', token)
+
+  // Cek apakah API diakses dengan menggunakan token
+  if (!token) {
+    console.log('Unauthorized Access : API Data Jurnal Cash')
+
+    return NextResponse.json({ error: 'Unauthorized Access' }, { status: 401 })
+  }
+
+  // Cek user Id yang mengakses API ini
+  const { searchParams } = new URL(req.url)
+  const userId = searchParams.get('userId')
+  const namaAkun = 'CASH'
+
+  if (!userId) {
+    return NextResponse.json({ error: 'User ID tidak ditemukan!' }, { status: 400 })
+  }
+
+  console.log('User dengan Id : ',userId,' mengakses API data jurnal')
+
+  // Ambil data jurnal dari database
+  try{
+    const jurnals = await prisma.jurnal.findMany({
+      where:{ akun:namaAkun},
+      select:{
+        id:true,
+        kode:true,
+        akun:true,
+        debit:true,
+        kredit:true,
+        updatedAt:true,
+      },
+    })
+
+    // Format tanggal update
+    const formatjurnal = jurnals.map(jurnal => ({
+      ...jurnal,
+      updatedAt: jurnal.updatedAt.toISOString()
+    }))
+
+    // Kirim data ke client
+    return NextResponse.json(formatjurnal, {status:200})
+
+  }
+  catch(error){
+
+  }
+
+}
