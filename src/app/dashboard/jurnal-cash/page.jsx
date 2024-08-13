c123bd5978f97e456f9866901770276af3c03520
Gesang Paudra J <g.paudra@gmail.com>
2024-08-13 12:35:25 +0700

    buat halaman jurnal cash

---
+++ src/app/dashboard/jurnal-cash/page.jsx
@@ -0,0 +1,43 @@
+
+'use client'
+
+import { useEffect, Suspense } from 'react'
+
+import { useRouter } from 'next/navigation'
+
+import { useSession } from 'next-auth/react'
+
+import TableJurnal from '@/views/jurnal/daftar-jurnal-cash'
+
+const HalamanJurnalCash = () =>{
+  const { data: session, status } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (status === 'loading') return // Jangan lakukan apa pun saat sesi sedang dimuat
+
+    if (!session) {
+      router.push('/error/401')
+    }
+  }, [session, status, router])
+
+  if (!session) {
+    return null
+  }
+
+  return(
+    <div>
+      <h1 className="text-2xl font-bold">
+        Jurnal Cash
+      </h1>
+      <br />
+      <Suspense fallback={<div>Memuat Data...</div>}>
+      <div>
+        <TableJurnal/>
+      </div>
+      </Suspense>
+    </div>
+  )
+}
+
+export default HalamanJurnalCash
